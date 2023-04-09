095beb1d4b761048013350424918ba7ae20a7bc8
Sofiya Ryabchikova <nikolay.ryabchikov@gmail.com>
2023-04-09 23:30:49 +0300

    Homework #4 is finished

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿// Homework 4
+
+// Task 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+/*
+ void Multiplication(int a, int b)
+ {
+     int result = a;
+     for (int i = 1; i < b; i++)
+         result = result * a;
+     
+     Console.WriteLine($"The number {a} in the degree {b} is {result}");
+ }
+
+ System.Console.Write("Input number <A>: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ System.Console.WriteLine("Input the degree <B> of the number <A>");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+ Multiplication (a, b);
+
+*/
+
+ // Task 27: Напишите программу, которая принимает на вход число и 
+ // выдаёт сумму цифр в числе.
+
+/*
+ int SumOfUnits(int num)
+ {
+     int result = 0;
+     for(int i = 0; num > 0; i++)
+     {
+         result += num % 10;
+         num /= 10;
+
+     }
+     return result;
+ }
+
+ System.Console.Write("Input your number: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+ System.Console.WriteLine(SumOfUnits(num));
+*/
+
+ // Task 29: Напишите программу, которая задаёт массив из 8 элементов и 
+ // выводит их на экран.
+
+/*
+
+ int[] GetRandomArray(int lenArray)
+ {
+     int[] array = new int[lenArray];
+     for (int i = 0; i < lenArray; i++)
+     {
+         array[i] = new Random().Next(1, 100);
+     }
+     return array;
+ }
+
+ void PrintArray(int[] array)
+ {
+     for (int i = 0; i < array.Length; i++)
+     {
+         Console.Write($"{array[i]} ");
+     }
+ }
+
+ PrintArray(GetRandomArray(8));
+*/
